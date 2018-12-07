ea8303f5f058d9129de1a5d6942fab3afad7eaae
Marcos Lessa <mlessa@gmail.com>
2018-12-07 14:47:07 +0000

    add dashboard settings

--- src/features/nav/Menus/SignedOutMenu.js
+++ src/features/nav/Menus/SignedOutMenu.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Menu, Button } from 'semantic-ui-react';
 
-const SignedOutMenu = () => {
+const SignedOutMenu = ({signIn}) => {
   return (
     <Menu.Item position="right">
-      <Button basic inverted content="Login" />
-      <Button basic inverted content="Sign Out" style={{ marginLeft: "0.5em" }} />
+      <Button onClick={signIn} basic inverted content="Login" />
+      <Button basic inverted content="Register" style={{ marginLeft: "0.5em" }} />
     </Menu.Item>
   );
 };
