8b6493f622c05c20adf14bdbdd5ae7badc0fae58
Marcos Lessa <mlessa@gmail.com>
2018-12-11 22:37:42 +0000

    add textarea component

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -5,6 +5,7 @@ import cuid from 'cuid';
 import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react';
 import { createEvent, updateEvent } from '../eventActions';
 import TextInput from '../../../app/common/form/TextInput';
+import TextArea from "../../../app/common/form/TextArea";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -72,7 +73,8 @@ class EventForm extends Component {
               <Field
                 name='description'
                 type='text'
-                component={TextInput}
+                rows={3}
+                component={TextArea}
                 placeholder='Tell us about your event'
               />
               <Header sub color='teal' content='Event Location Details' />
