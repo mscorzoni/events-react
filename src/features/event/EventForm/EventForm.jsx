1096bb0087566e80b0f66f4f6fba42a321f6ffd1
Marcos Lessa <mlessa@gmail.com>
2018-12-11 23:09:03 +0000

    add a reusable selection field

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -6,10 +6,11 @@ import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react';
 import { createEvent, updateEvent } from '../eventActions';
 import TextInput from '../../../app/common/form/TextInput';
 import TextArea from "../../../app/common/form/TextArea";
+import SelectInput from "../../../app/common/form/SelectInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
-
+  
   let event = {
     title: '',
     date: '',
@@ -17,7 +18,7 @@ const mapState = (state, ownProps) => {
     venue: '',
     hostedBy: ''
   }
-
+  
   if(eventId && state.events.length > 0) {
     event = state.events.filter(event => event.id === eventId )[0];
   }
@@ -25,6 +26,15 @@ const mapState = (state, ownProps) => {
     event
   }
 }
+
+const category = [
+	{ key: "drinks", text: "Drinks", value: "drinks" },
+	{ key: "culture", text: "Culture", value: "culture" },
+	{ key: "film", text: "Film", value: "film" },
+	{ key: "food", text: "Food", value: "food" },
+	{ key: "music", text: "Music", value: "music" },
+	{ key: "travel", text: "Travel", value: "travel" }
+];
 
 const actions = {
   createEvent,
@@ -51,6 +61,7 @@ class EventForm extends Component {
   }
 
 
+
   render() {
     return (
       <Grid>
@@ -67,7 +78,8 @@ class EventForm extends Component {
               <Field
                 name='category'
                 type='text'
-                component={TextInput}
+                component={SelectInput}
+                options={category}
                 placeholder='What is your event about'
               />
               <Field
