fdf377f5f2051fdfd8c3c6e18e6465b4bf083d68
Marcos Lessa <mlessa@gmail.com>
2018-12-16 11:47:09 +0000

    hook up register with firebase

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -1,6 +1,6 @@
 import { SubmissionError } from 'redux-form';
-import { SIGN_OUT_USER } from './authConstants';
 import { closeModal } from '../modals/modalActions';
+
 
 export const login = (creds) => {
   return async( dispatch, getState, {getFirebase}) => {
@@ -17,8 +17,24 @@ export const login = (creds) => {
   }
 }
 
-export const logout = () => {
-  return {
-    type: SIGN_OUT_USER
+export const registerUser = (user) =>
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    try {
+      let createdUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password);
+      await createdUser.updateProfile({
+        displayName: user.displayName
+      })
+      let newUser = {
+        displayName: user.displayName,
+        createdAt: firebase.FieldValue.serverTimestamp()
+      }
+      await firestore.set(`user/${createdUser.id}, {...newUser}`);
+      dispatch(closeModal());
+    } catch(error){
+      console.log(error)
+    }
   }
-}
