7715a73d7d9b367640aa68750e3d28f180212fa0
Marcos Lessa <mlessa@gmail.com>
2018-12-17 15:16:22 +0000

    add gender option as radio input

--- src/features/user/Settings/BasicPage.jsx
+++ src/features/user/Settings/BasicPage.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Segment, Form, Header, Divider, Button } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
+import moment from 'moment';
 import DateInput from '../../../app/common/form/DateInput';
 import PlaceInput from "../../../app/common/form/PlaceInput";
 import TextInput from "../../../app/common/form/TextInput";
+import RadioInput from "../../../app/common/form/RadioInput";
 
 class BasicPage extends Component {
   render() {
@@ -20,13 +22,32 @@ class BasicPage extends Component {
             placeholder='Known As' 
           />
           <Form.Group inline >
-
+            <label>Gender:</label>
+            <Field 
+              name='gender'
+              type='radio'
+              value='male'
+              label='Male'
+              component={RadioInput}
+            />
+            <Field
+              name='gender'
+              type='radio'
+              value='female'
+              label='Female'
+              component={RadioInput}
+            />
           </Form.Group>
           <Field
             width={8}
             name='dateOfBirth'
             component={DateInput}
             placeholder='Date of Birth'
+            dateFormat='YYYY-MM-DD'
+            showYearDropdown={true}
+            showMonthDropdown={true}
+            dropdownMode='select'
+            maxDate={moment().subtract(18, 'years')}
           />
           <Field
             options={{ types: ['(cities)'] }}
