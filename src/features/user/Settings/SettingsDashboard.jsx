22f24948ba4e5b55d24dca0261806371b1693a54
Marcos Lessa <mlessa@gmail.com>
2018-12-17 16:06:53 +0000

    updating user profile  at firebase

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -8,9 +8,11 @@ import PhotosPage from './PhotosPage'
 import AccountPage from './AccountPage'
 import BasicPage from './BasicPage'
 import { updatePassword } from '../../auth/authActions';
+import { updateProfile } from '../userAction';
 
 const actions = {
-  updatePassword
+  updatePassword,
+  updateProfile
 }
 
 const mapState = (state) => ({
@@ -19,13 +21,13 @@ const mapState = (state) => ({
   user: state.firebase.profile
 })
 
-const SettingsDashboard = ({updatePassword, providerId, user}) => {
+const SettingsDashboard = ({updatePassword, providerId, user, updateProfile }) => {
   return (
     <Grid>
       <Grid.Column width={12}>
         <Switch>
           <Redirect exact from='/settings' to='/settings/basic'/>
-          <Route path='/settings/basic' render={() => <BasicPage  initialValues={user} />}/>
+          <Route path='/settings/basic' render={() => <BasicPage updateProfile={updateProfile} initialValues={user} />}/>
           <Route path='/settings/about' component={AboutPage}/>
           <Route path='/settings/photos' component={PhotosPage}/>
           <Route 
