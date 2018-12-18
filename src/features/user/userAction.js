177b56ee14e52a8a36114eb2433d702d1af96a79
Marcos Lessa <mlessa@gmail.com>
2018-12-18 14:03:47 +0000

    create photo upload action

--- src/features/user/userAction.js
+++ src/features/user/userAction.js
@@ -15,3 +15,38 @@ export const updateProfile = (user) =>
       
     }
   }
+
+  export const updateProfileImage = (file, fileName) => 
+    async (dispatch, getState, {getFirebase, getFirestore}) => {
+      const firebase = getFirebase();
+      const firestore = getFirestore();
+      const user =  firebase.auth().currentUser;
+      const path = `${user.uid}/user_images`;
+      const options = {
+        name: fileName
+      }
+      try {
+        let uploadedFile = await firebase.uploadedFile(path, file, null, options);
+        let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+        let userDoc = await firestore.get(`users/${user.uid}`);
+        if(!userDoc.data().photoURL){
+          await firebase.updateProfile({
+            photoURL: downloadURL
+          })
+          await user.updateProfile({
+            photoURL: downloadURL
+          })
+        }
+        return await firestore.add({
+          collection: 'users',
+          doc: user.uid,
+          subcollections: [{collection: 'photos'}]
+        }, {
+          name: fileName,
+          url: downloadURL
+        })
+      } catch(error){
+        console.log(error)
+        throw new Error('Problem uploading photo')
+      }
+    }
