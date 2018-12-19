1e79fdeb8394a5a75d169269b86dbfe0e886b963
Marcos Lessa <mlessa@gmail.com>
2018-12-19 19:23:58 +0000

    display upload photo

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -26,7 +26,8 @@ const actions = {
 
 const mapState = (state) => ({
   auth: state.firebase.auth,
-  profile: state.firebase.profile
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos
 })
 
 class PhotosPage extends Component {
@@ -75,6 +76,14 @@ class PhotosPage extends Component {
   }
 
   render() {
+    const {photos, profile} = this.props;
+    let filteredPhotos;
+    if (photos) {
+      filteredPhotos = photos.filter(photo => {
+        return photo.url !== profile.photoURL
+      })
+    }
+
     return (
       <Segment>
         <Header dividing size='large' content='Your Photos' />
@@ -127,19 +136,20 @@ class PhotosPage extends Component {
 
         <Card.Group itemsPerRow={5} >
           <Card>
-            <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
+            <Image src={profile.photoURL} />
             <Button positive>Main Photo</Button>
           </Card>
-
-          <Card>
-            <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-            <div className="ui two buttons">
-              <Button basic color="green" >
-                Main
+          { photos && filteredPhotos.map((photo) => (
+            <Card key={photo.id}>
+              <Image src={photo.url} />
+              <div className="ui two buttons">
+                <Button basic color="green" >
+                  Main
               </Button>
-              <Button basic icon='trash' color='red' />
-            </div>
-          </Card>
+                <Button basic icon='trash' color='red' />
+              </div>
+            </Card>
+          ))}  
         </Card.Group>
       </Segment>
     );
