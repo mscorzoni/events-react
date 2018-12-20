4f4c7919126a8cdf6e05f971d682245bb3b4d195
Marcos Lessa <mlessa@gmail.com>
2018-12-20 17:28:40 +0000

    add upload circle

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -29,7 +29,8 @@ const actions = {
 const mapState = (state) => ({
   auth: state.firebase.auth,
   profile: state.firebase.profile,
-  photos: state.firestore.ordered.photos
+  photos: state.firestore.ordered.photos,
+  loading: state.async.loading
 })
 
 class PhotosPage extends Component {
@@ -95,7 +96,7 @@ class PhotosPage extends Component {
   }
 
   render() {
-    const {photos, profile} = this.props;
+    const {photos, profile, loading} = this.props;
     let filteredPhotos;
     if (photos) {
       filteredPhotos = photos.filter(photo => {
@@ -142,8 +143,18 @@ class PhotosPage extends Component {
             <div>
               <Image style={{ minHeight: '200px', minWidth: '200px' }} src={this.state.cropResult} />  
               <Button.Group>
-                <Button onClick={this.uploadImage} style={{width: '100px'}} positive icon='check' />
-                <Button onClick={this.cancelCrop}  style={{width: '100px'}} icon='close' />
+                <Button
+                  loading={loading} 
+                  onClick={this.uploadImage}
+                  style={{width: '100px'}}
+                  positive icon='check'
+                  />
+                <Button
+                  disabled={loading}
+                  onClick={this.cancelCrop}
+                  style={{width: '100px'}}
+                  icon='close'
+                  />
               </Button.Group>    
             </div>
             )}
@@ -155,14 +166,17 @@ class PhotosPage extends Component {
 
         <Card.Group itemsPerRow={5} >
           <Card>
-            <Image src={profile.photoURL} />
+            <Image src={profile.photoURL || '/assets/user.png'} />
             <Button positive>Main Photo</Button>
           </Card>
           { photos && filteredPhotos.map((photo) => (
             <Card key={photo.id}>
               <Image src={photo.url} />
               <div className="ui two buttons">
-                <Button onClick={this.handleSetMainPhoto(photo)} basic color="green" >
+                <Button 
+                    onClick={this.handleSetMainPhoto(photo)}
+                    basic
+                    color="green" >
                   Main
               </Button>
                 <Button  onClick={this.handlePhotoDelete(photo)} basic icon='trash' color='red' />
